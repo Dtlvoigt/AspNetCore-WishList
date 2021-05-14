32e1844e679e3c4267a384f6e17251b526caa5a7
Dylan Voigt <DVOIGT@yaculta.com>
2021-05-14 09:45:30 -0700

    fixed typo

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -29,7 +29,7 @@ namespace WishList
                 app.UseExceptionHandler("/Home/Error");
 
             app.UseRouting();
-            app.UseEndpoints(endopints => { endopints.MapDefaultControllerRoute(); }
+            app.UseEndpoints(endopints => { endopints.MapDefaultControllerRoute(); });
         }
     }
 }
